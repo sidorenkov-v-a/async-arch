20f1ecdfe7529fbe1600d37e703d771642eaf8cb
Vladislav Sidorenkov <vasidorenkov@avito.ru>
2024-02-29 10:20:56 +0800

    add makefile

--- boilerplate/api/generated/server.go
+++ boilerplate/api/generated/server.go
@@ -11,37 +11,6 @@ import (
 	"github.com/oapi-codegen/runtime"
 )
 
-// Error defines model for Error.
-type Error struct {
-	Code    int32  `json:"code"`
-	Message string `json:"message"`
-}
-
-// NewPet defines model for NewPet.
-type NewPet struct {
-	Name string  `json:"name"`
-	Tag  *string `json:"tag,omitempty"`
-}
-
-// Pet defines model for Pet.
-type Pet struct {
-	Id   int64   `json:"id"`
-	Name string  `json:"name"`
-	Tag  *string `json:"tag,omitempty"`
-}
-
-// FindPetsParams defines parameters for FindPets.
-type FindPetsParams struct {
-	// Tags tags to filter by
-	Tags *[]string `form:"tags,omitempty" json:"tags,omitempty"`
-
-	// Limit maximum number of results to return
-	Limit *int32 `form:"limit,omitempty" json:"limit,omitempty"`
-}
-
-// AddPetJSONRequestBody defines body for AddPet for application/json ContentType.
-type AddPetJSONRequestBody = NewPet
-
 // ServerInterface represents all server handlers.
 type ServerInterface interface {
 
