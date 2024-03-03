a648bf9707427ba0e51537178844e3ec7613fb4c
Vladislav Sidorenkov <vasidorenkov@avito.ru>
2024-03-03 16:55:02 +0800

    drop databus example

--- boilerplate/cmd/service/main.go
+++ boilerplate/cmd/service/main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/signal"
-	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/segmentio/kafka-go"
-	"golang.org/x/sync/errgroup"
 
 	"async-arch/boilerplate/internal/infrastructure/contract"
 	"async-arch/boilerplate/internal/infrastructure/di"
-	databus3 "async-arch/boilerplate/pkg/databus"
-	"async-arch/boilerplate/pkg/env"
 )
 
 const (
@@ -78,98 +72,8 @@ func run(ctx context.Context, log contract.Log) (err error) {
 	//https://github.com/deepmap/oapi-codegen/tree/master/examples/petstore-expanded/gorilla
 	r := mux.NewRouter()
 
-	// Databus test
-
-	// make a new reader that consumes from topic-A
-
-	err = databusExample(ctx, env, log)
-	if err != nil {
-		return err
-	}
-
 	// Run API Server
 	apiServer := di.NewAPIServer(&config.APIServer)
 
 	return apiServer.Run(r)
-}
-
-func databusExample(ctx context.Context, env *env.Config, log contract.Log) error {
-	databus := di.NewDatabus(env.Databus, log)
-
-	producer1 := di.NewProducer(databus, "topic_test_1")
-	producer2 := di.NewProducer(databus, "topic_test_2")
-
-	var handler1 databus3.ConsumerHandler = func(ctx context.Context, message kafka.Message) error {
-		fmt.Println("-------------------------")
-		fmt.Println("Handler 1 receive message")
-		fmt.Println(message)
-
-		return nil
-	}
-
-	var handler2 databus3.ConsumerHandler = func(ctx context.Context, message kafka.Message) error {
-		fmt.Println("-------------------------")
-		fmt.Println("Handler 2 receive message")
-		fmt.Println(message)
-
-		return nil
-	}
-
-	consumer1 := di.NewConsumer(databus, "topic_test_1", "consumer_1", handler1)
-	consumer2 := di.NewConsumer(databus, "topic_test_2", "consumer_2", handler2)
-
-	go func() {
-		for {
-
-			time.Sleep(1 * time.Second)
-
-			msgs1 := []*databus3.Message{
-				{
-					Key:     "key1",
-					Payload: []byte(time.Now().String()),
-				},
-				{
-					Key:     "key2",
-					Payload: []byte(time.Now().String()),
-				},
-			}
-
-			msgs2 := []*databus3.Message{
-				{
-					Key:     "key3",
-					Payload: []byte(time.Now().String()),
-				},
-				{
-					Key:     "key4",
-					Payload: []byte(time.Now().String()),
-				},
-			}
-
-			err := producer1.Produce(ctx, msgs1...)
-			if err != nil {
-				fmt.Println("Error", err)
-			}
-
-			err = producer2.Produce(ctx, msgs2...)
-			if err != nil {
-				fmt.Println("Error", err)
-			}
-		}
-	}()
-
-	g, gCtx := errgroup.WithContext(ctx)
-
-	g.Go(func() (err error) {
-		return consumer1.Consume(gCtx)
-	})
-
-	g.Go(func() (err error) {
-		return consumer2.Consume(gCtx)
-	})
-
-	if err := g.Wait(); err != nil {
-		return err
-	}
-
-	return nil
 }
